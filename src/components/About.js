43c49eebac58851a9898e080ac63bf6ff31abae2
Adam White <adamfrankwhite@gmail.com>
2020-12-08 18:28:38 +0000

    update about

--- src/components/About.js
+++ src/components/About.js
@@ -9,7 +9,7 @@ export default function About() {
                 at a glance, I created a simple app to convert KDP royalties to
                 a chosen currency. I'll be extending the functionality in due
                 course. Any queries, contact me{" "}
-                <a href="info@adamwhite.tech">here</a>
+                <a href="mailto:info@adamwhite.tech">here</a>
             </p>
             {/* </div> */}
         </section>
